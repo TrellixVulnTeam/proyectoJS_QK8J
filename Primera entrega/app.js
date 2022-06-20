44c22443bef8e29abc9e84cbaf19a7980a0bbe7d
Paola Gonzalez <paogm91@gmail.com>
2022-06-20 17:55:43 -0300

    DOM

--- Primera entrega/app.js
+++ Primera entrega/app.js
@@ -1,55 +1,33 @@
 
-// TIENDA VIRTUAL - CONSULTORIA
-/*
-const perfume = [  {nombre: "Kaiak Clasico", caracteristicas: "Masculino", precio: 4800},  
-                    {nombre: "Kaiak Aventura", caracteristicas: "Femenino", precio: 4800},       
-                    {nombre: "Dose de Humor", caracteristicas: "Femenino", precio: 3600},
-                    {nombre: "Beijo de Humor", caracteristicas: "Femenino", precio: 3600}    ];
+const titulo = document.getElementById("titulo")
+const subtitulo= document.getElementById("subtitulo")
 
-const jabones = [   {nombre: "Tododia", formato: "barra", aroma: "Flor de Manzana", precio: 700},
-                    {nombre: "Tododia", formato: "barra", aroma: "Cereza y Avellana", precio: 700},
-                    {nombre: "Tododia", formato: "barra", aroma: "Flor de Lis", precio: 700},
-                    {nombre: "Tododia", formato: "liquido", aroma: "Frambuesa y Pimienta Rosa", precio: 700}
+titulo.innerText= "NATbyPG"
+subtitulo.innerText= "Consultora Natura"
 
-]
 
-const busca = prompt ("Que producto esta buscando? \n Perfumes \n Cremas \n Jabones").toLowerCase();
-if (busca == "perfumes".toLowerCase()) {
-    const busca1 = prompt ("Busca perfume \n Femenino \n Masculino").toLowerCase();
-    
-    if (busca1 == "femenino") {
-        const fem = perfume.filter (producto => producto.caracteristicas == "Femenino");
-        console.log (fem);
-    } else if (busca1 == "masculino") {
-        const masc = perfume.filter (producto => producto.caracteristicas == "Masculino");
-        console.log (masc);
-    } else {
-        alert ("Usted ha ingresado un termino no valido");
-        prompt ("Busca perfume \nFemenino \n Masculino?").toLowerCase();
-    }
-*/
-    //Quiero filtrar sobre el resultado de busca1, como?
+const perfumes = [  {id: 1, nombre: "Kaiak Clasico", caracteristicas: "Masculino", precio: 4800},  
+                    {id: 2, nombre: "Kaiak Aventura", caracteristicas: "Femenino", precio: 4800},       
+                    {id: 3, nombre: "Dose de Humor", caracteristicas: "Femenino", precio: 3600},
+                    {id: 4, nombre: "Beijo de Humor", caracteristicas: "Femenino", precio: 3600}    ];
 
-      /*  const busca2 = prompt ("Que precio aproximado?");
-
-        if (busca2 <= 4000) {
-            const menos4k = fem.filter (producto => producto.precio <= 4000);
-            console.log (menos4k)
-        } */
-/*
-} else if (busca == "cremas".toLowerCase()) {
-    console.log ("No quedaron cremas disponibles")
-} else if (busca == "jabones".toLowerCase()) {
-    const busca1 = prompt ("\n Liquido \n En Barra")
-
-    if (busca1 == "liquido".toLowerCase()) {
-        const liquido = jabones.filter (producto => producto.formato == "liquido")
-        console.log (liquido)
-    } else if (busca1 == "en barra".toLowerCase()) {
-        const barra = jabones.filter (producto => producto.formato == "barra")
-        console.log (barra)
-    }
-} else {
-    alert ("El producto que usted busca no esta en stock actualmente. Por favor envia un mensaje para consultar futura disponibilidad o para pedirlo por encargo.")
+for (const perfume of perfumes) {
+    let contenedor = document.createElement("section");
+    contenedor.innerHTML = `<h3> Perfume: ${perfume.nombre}</h3>
+                            <p> ${perfume.caracteristicas} </p>
+                            <b> $ ${perfume.precio}</b>`;
+    document.body.appendChild(contenedor); 
 }
- */
+const jabones = [   {id: 5, nombre: "Tododia", formato: "Barra", aroma: "Flor de Manzana", precio: 700},
+                    {id: 6, nombre: "Tododia", formato: "Barra", aroma: "Cereza y Avellana", precio: 700},
+                    {id: 7, nombre: "Tododia", formato: "Barra", aroma: "Flor de Lis", precio: 700},
+                    {id: 8, nombre: "Tododia", formato: "Liquido", aroma: "Frambuesa y Pimienta Rosa", precio: 700} ]
+
+ for (const jabon of jabones) {
+    let contenedor = document.createElement("section");
+        contenedor.innerHTML = `<h3> Jabon: ${jabon.nombre}</h3>
+                                <p> ${jabon.formato}</p>
+                                <p> ${jabon.aroma} </p>
+                                <b> $ ${jabon.precio}</b>`;
+    document.body.appendChild(contenedor); 
+}
